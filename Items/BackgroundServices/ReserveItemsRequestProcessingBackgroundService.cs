1831a03a543e5ebec50e1a775a1b9edf52b00b8a
Ivan-Timofeev <it446374@mindbox.cloud>
2023-12-14 01:58:15 +0300

    Сделать так что бы МКС не был "жестко" завязан на RabbitMQ. Не получилось достучаться? Пишем в логи ошибку. Не падаем полностью

--- Items/BackgroundServices/ReserveItemsRequestProcessingBackgroundService.cs
+++ Items/BackgroundServices/ReserveItemsRequestProcessingBackgroundService.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
 using Items.Models.DataTransferObjects.CreateOrder;
@@ -12,8 +11,9 @@ public sealed class ReserveItemsRequestProcessingBackgroundService : BackgroundS
 {
     private readonly ILogger<ReserveItemsRequestProcessingBackgroundService> _logger;
     private readonly IReserveItemsRequestProcessor _reserveItemsRequestProcessor;
-    private readonly IConnection _connection;
-    private readonly IModel _channel;
+    private readonly IConfiguration _configuration;
+    private IConnection? _connection;
+    private IModel? _channel;
 
     public ReserveItemsRequestProcessingBackgroundService(
         ILogger<ReserveItemsRequestProcessingBackgroundService> logger,
@@ -22,48 +22,75 @@ public sealed class ReserveItemsRequestProcessingBackgroundService : BackgroundS
     {
         _logger = logger;
         _reserveItemsRequestProcessor = reserveItemsRequestProcessor;
-
-        var factory = new ConnectionFactory { HostName = configuration["RabbitMqHostName"] };
-        _connection = factory.CreateConnection();
-        _channel = _connection.CreateModel();
-
-        _channel.QueueDeclare(
-            queue: "ReserveItemsRequest",
-            durable: true,
-            exclusive: false,
-            autoDelete: false,
-            arguments: null);
-        
-        _logger.LogInformation("Initialization was successful. Waiting for messages in the queue");
+        _configuration = configuration;
     }
 
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        stoppingToken.ThrowIfCancellationRequested();
-
-        var consumer = new EventingBasicConsumer(_channel);
-        consumer.Received += (ch, eventArgs) =>
+        Task.Run(() =>
         {
-            var json = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
-            var createOrderRequest = JsonSerializer.Deserialize<ReserveItemsRequest>(json)
-                ?? throw new InvalidOperationException("Json must be of type CreateOrderRequest");
+            InitializeRabbitMq();
 
-            _logger.LogInformation(
-                "Started processing of CreateOrderRequest. TransactionalId: {T}",
-                createOrderRequest.TransactionalId);
+            var consumer = new EventingBasicConsumer(_channel);
 
-            _reserveItemsRequestProcessor.ProcessReserveItemsRequest(createOrderRequest);
+            consumer.Received += (ch, eventArgs) =>
+            {
+                var json = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+                var reserveItemsRequest = JsonSerializer.Deserialize<ReserveItemsRequest>(json)
+                    ?? throw new InvalidOperationException("Json must be of type ReserveItemsRequest");
 
-            _logger.LogInformation(
-                "CreateOrderRequest processed. TransactionalId: {T}",
-                createOrderRequest.TransactionalId);
+                _logger.LogInformation(
+                    "Started processing of ReserveItemsRequest. OrderId: {O}",
+                    reserveItemsRequest.OrderId);
 
-            _channel.BasicAck(eventArgs.DeliveryTag, false);
-        };
+                _reserveItemsRequestProcessor.ProcessReserveItemsRequest(reserveItemsRequest);
 
-        _channel.BasicConsume("ReserveItemsRequest", false, consumer);
+                _logger.LogInformation(
+                    "ReserveItemsRequest processed. OrderId: {O}",
+                    reserveItemsRequest.OrderId);
+
+                _channel!.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            _channel!.BasicConsume("ReserveItemsRequest", false, consumer);
+        }, cancellationToken);
 
         return Task.CompletedTask;
+    }
+
+    private void InitializeRabbitMq()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = _configuration["RabbitMqHostName"],
+            AutomaticRecoveryEnabled = true
+        };
+
+        while (_channel is null)
+        {
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+
+                _channel.QueueDeclare(
+                    queue: "ReserveItemsResponse",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(
+                    "Initialization error. Retrying after 120s\nMessage: {M}",
+                    ex.Message);
+
+                Thread.Sleep(120_000);
+            }
+        }
+
+        _logger.LogInformation("Initialization was successful. Waiting for messages in the queue");
     }
 
     public override void Dispose()
--- Items/Services/OrdersMicroserviceApiClient.cs
+++ Items/Services/OrdersMicroserviceApiClient.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
 using Items.Models.DataTransferObjects.CreateOrder;
@@ -13,12 +12,38 @@ public interface IOrdersMicroserviceApiClient
 
 public sealed class OrdersMicroserviceApiClient : IOrdersMicroserviceApiClient, IDisposable
 {
-    private readonly IConnection _connection;
-    private readonly IModel _model;
+    private readonly IConfiguration _configuration;
+    private IConnection? _connection;
+    private IModel? _model;
 
     public OrdersMicroserviceApiClient(IConfiguration configuration)
     {
-        var factory = new ConnectionFactory { HostName = configuration["RabbitMqHostName"] };
+        _configuration = configuration;
+    }
+
+    public void MakeResponseForReserveItemsRequest(ReserveItemsResponse response)
+    {
+        if (_model is null || _connection is null)
+            InitializeRabbitMq();
+        
+        var json = JsonSerializer.Serialize(response);
+        var body = Encoding.UTF8.GetBytes(json);
+
+        _model!.BasicPublish(exchange: "",
+            routingKey: "ReserveItemsResponse",
+            basicProperties: null,
+            body: body);
+    }
+
+    public void Dispose()
+    {
+        _connection?.Dispose();
+        _model?.Dispose();
+    }
+
+    private void InitializeRabbitMq()
+    {
+        var factory = new ConnectionFactory { HostName = _configuration["RabbitMqHostName"] };
         _connection = factory.CreateConnection();
         _model = _connection.CreateModel();
 
@@ -27,22 +52,5 @@ public sealed class OrdersMicroserviceApiClient : IOrdersMicroserviceApiClient, 
             exclusive: false,
             autoDelete: false,
             arguments: null);
-    }
-
-    public void MakeResponseForReserveItemsRequest(ReserveItemsResponse response)
-    {
-        var json = JsonSerializer.Serialize(response);
-        var body = Encoding.UTF8.GetBytes(json);
-
-        _model.BasicPublish(exchange: "",
-            routingKey: "ReserveItemsResponse",
-            basicProperties: null,
-            body: body);
-    }
-
-    public void Dispose()
-    {
-        _connection.Dispose();
-        _model.Dispose();
     }
 }
