fb684835511dc6291ba541b4d0024c1fd1e0886e
Ivan-Timofeev <it446374@mindbox.cloud>
2023-12-14 05:29:05 +0300

    ,

--- Items/Services/ReserveItemsRequestProcessor.cs
+++ Items/Services/ReserveItemsRequestProcessor.cs
@@ -39,8 +39,8 @@ public class ReserveItemsRequestProcessor : IReserveItemsRequestProcessor
             .Items
             .GetItems(itemIds);
 
-        int a = 100;
-        if (a == 100)
+        int a = 102;
+        if (a == 102)
             throw new Exception("Test ci/cd");
 
         if (foundItems.Count != requestedItemsIds.Length)
